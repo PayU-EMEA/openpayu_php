ea1f3e766804b0d598028a49346f126a6b9ccf47
gniewkoo <gniewkostrowski@gmail.com>
2014-04-02 10:58:14 +0200

    Fix for OpenPayU_Util::getRequestHeaders()

--- lib/OpenPayU/Util.php
+++ lib/OpenPayU/Util.php
@@ -261,7 +261,17 @@ class OpenPayU_Util
      */
     public static function getRequestHeaders()
     {
-        return apache_request_headers();
+        if(!function_exists('apache_request_headers')) {
+                $headers = array();
+                foreach($_SERVER as $key => $value) {
+                    if(substr($key, 0, 5) == 'HTTP_') {
+                        $headers[str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))))] = $value;
+                    }
+                }
+                return $headers;
+        }else{
+            return apache_request_headers();
+        }
     }
 
     /**
