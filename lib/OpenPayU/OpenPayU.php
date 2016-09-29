9267315b1602402976e33449b2f2aad58a2de0b4
unknown <bartosz.zielinski@POLPC00196.allegrogroup.internal>
2016-09-29 09:51:12 +0200

    Change Status WARNING_CONTINUE_REDIRECT to SUCCESS

--- lib/OpenPayU/OpenPayU.php
+++ lib/OpenPayU/OpenPayU.php
@@ -13,6 +13,12 @@ class OpenPayU
     protected static function build($data)
     {
         $instance = new OpenPayU_Result();
+
+        if($data['status']=='WARNING_CONTINUE_REDIRECT'){
+            $data['status']='SUCCESS';
+            $data['response']['status']['statusCode']='SUCCESS';
+        }
+
         $instance->init($data);
 
         return $instance;
