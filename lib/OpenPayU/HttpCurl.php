40f55a6b0253dddbc0c33d5d20e1c833a14f020a
Tomasz Regdos <tomasz.regdos@payu.pl>
2021-07-13 09:26:39 +0200

    Fix #117 - Default for CURLOPT_SSL_VERIFYHOST (2) and CURLOPT_SSL_VERIFYPEER (true)

--- lib/OpenPayU/HttpCurl.php
+++ lib/OpenPayU/HttpCurl.php
@@ -42,8 +42,6 @@ class OpenPayU_HttpCurl
         }
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_TIMEOUT, 60);
 
         if ($proxy = self::getProxy()) {
